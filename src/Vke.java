68b285c45e6049fb0a73553de8ce3208013ca44a
onurcanx <onuraa79@gmail.com>
2025-10-07 14:12:35 +0300

    Add Vke.java

---
+++ src/Vke.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Vke {
+    static void main(String[] args) {
+        Scanner imp = new Scanner(System.in);
+        double boy, kilo,sonuc;
+        System.out.print("Boyunuzu metre cinsinden giriniz:");
+        boy= imp.nextDouble();
+        System.out.print("kilonuzu kg cinsinden giriniz:");
+        kilo= imp.nextDouble();
+        sonuc=kilo/(boy*boy);
+        System.out.print("Vücut kitle endeksiniz:"+sonuc);
+    }
+}
